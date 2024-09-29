72fa769975765cd39bd99a37becd7666a49cc074
mintRaven-05 <debjeetbanerjee48@gmail.com>
2024-09-29 11:13:24 +0530

    [GenAI]: image processor completed

--- gemini_assistant/image.py
+++ gemini_assistant/image.py
@@ -1,6 +1,7 @@
 # ------------------imports-----------------
 import os
 import platform
+from chat import init_model
 import google.generativeai as genai
 
 # ------------------------------------------
@@ -8,3 +9,10 @@ if platform.architecture()[1] == "ELF":
     os.environ["GRPC_VERBOSITY"] = "ERROR"
     os.environ["GLOG_minloglevel"] = "2"
 # ------------------------------------------
+
+
+def process_image(image, query: str) -> str:
+    model = init_model("gemini-1.5-flash", "")
+    img_file = genai.upload_file(image)
+    result = model.generate_content([img_file, "\n\n", query])
+    return result.text
