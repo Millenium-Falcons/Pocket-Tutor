2f864a312ee718038be9368b0f9ca867dd62181f
mintRaven-05 <debjeetbanerjee48@gmail.com>
2024-09-29 12:17:33 +0530

    [GenAI]: removed unnecessary comments

--- gemini_assistant/deploy.py
+++ gemini_assistant/deploy.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, File, Form, UploadFile, HTTPException
 import os
-import tempfile  # To create a temporary file
+import tempfile
 
 app = FastAPI()
 
@@ -13,21 +13,16 @@ def index():
 @app.post("/doc")
 async def pdoc(doc: UploadFile = File(...), query: str = Form(...)):
     try:
-        # Create a temporary file to store the uploaded document
         with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp_file:
-            temp_file.write(await doc.read())  # Write the file content to the temp file
-            temp_file_path = temp_file.name  # Get the path of the temp file
+            temp_file.write(await doc.read())
+            temp_file_path = temp_file.name
 
-        # Process the document using its path
         res = process_doc(temp_file_path, query)
-
-        # Return the response
         return {"response": res}
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
     finally:
-        # Delete the temporary file after processing
         if os.path.exists(temp_file_path):
             os.remove(temp_file_path)
