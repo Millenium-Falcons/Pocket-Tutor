f4f655388d0516e4c4aa149b32c07deed7cc7e82
mintRaven-05 <debjeetbanerjee48@gmail.com>
2024-09-30 09:54:19 +0530

    [GenAI]: add meta-data to chat.py

--- gemini_assistant/chat.py
+++ gemini_assistant/chat.py
@@ -1,21 +1,32 @@
-# ----------------imports------------------
+# created by: mintRaven-05
+# dated: 29 sept 2024
+#       This module is responsible for generating text based
+#       responses using the API of Google Gemini. Chat session with
+#       pre-loaded history...history of last 10 chats with
+#       Google Gemini.
+#
+# -----------------------------------------------------------------------------------
 import os
 import platform
 from sys import exception
 from globals.history import *
 import google.generativeai as genai
+# -----------------------------------------------------------------------------------
 
-# -----------------------------------------
 if platform.architecture()[1] == "ELF":
     os.environ["GRPC_VERBOSITY"] = "ERROR"
     os.environ["GLOG_minloglevel"] = "2"
-# ------------------------------------------
+
+# -----------------------------------------------------------------------------------
 
 
 def init_model(model_name: str, API: str) -> genai.GenerativeModel:
     genai.configure(api_key=API)
     model = genai.GenerativeModel(model_name)
     return model
+
+
+# -----------------------------------------------------------------------------------
 
 
 def chat_session(query: str) -> str:
@@ -27,3 +38,6 @@ def chat_session(query: str) -> str:
         return response.text
     except Exception as e:
         print(f"error: {e}")
+
+
+# -----------------------------------------------------------------------------------
