bf8b4f8543f174a01518630132e1ba30897a0611
Hritesh_Saha <hriteshsaha4@gmail.com>
2024-10-02 22:46:48 +0530

    [Backend]-making required false in profile pic

--- backend/Schema/profile.js
+++ backend/Schema/profile.js
@@ -3,9 +3,11 @@ const mongoose=require("mongoose");
 const profileSchema= new mongoose.Schema({
     profilePicture: {
         type: Buffer, // Store the binary image data
+        required: false,
       },
       profilePictureType: {
         type: String, // Store the image type (e.g., 'image/jpeg' or 'image/png')
+        required: false,
       },
     username:{
         type:String,
